558e51ad2b71794c112b740cb144bd37b3d396c9
matsumoto-ops <matsu8114@gmail.com>
2020-08-10 13:16:36 +0900

    version修正

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
 }
 
 terraform {
-  required_version = "0.12.8"
+  required_version = "0.12.29"
   backend "s3" {
-    bucket   = "terraform-state-storage-kensyu"
-    key      = "terraform.tfstate"
-    profile  = "dev-user"  # ~/.aws/credentials
-    region   = "ap-northeast-1"
+    bucket  = "terraform-state-storage-kensyu"
+    key     = "terraform.tfstate"
+    profile = "dev-user" # ~/.aws/credentials
+    region  = "ap-northeast-1"
     encrypt = true
 
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,9 +5,9 @@ variable "aws_account_id" {
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "profile_name" {}
-variable "backend_bucket_name" { 
-    default = "terraform-state-storage-kensyu"
- }
+variable "backend_bucket_name" {
+  default = "terraform-state-storage-kensyu"
+}
 
 variable "aws_region" {
   default = "ap-northeast-1"
